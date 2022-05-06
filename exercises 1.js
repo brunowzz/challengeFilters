30da552ac2c46526d66d8c37875ce4b8d153af10
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2022-05-06 20:06:07 -0300

    second commit

--- exercises 1.js
+++ exercises 1.js
@@ -3,5 +3,8 @@
 const list = [20, 3, 234, 12, 17, 541, 6, 87, 275, 1000]
 
 const newList = list.filter((number) => {
-    
+    if(number % 2 == 0 && number % 5 == 0) 
+    return number
 })
+
+console.log(newList)
--- exercises 2.js
+++ exercises 2.js
@@ -1,17 +1,21 @@
 // USAR O FILTER para identificar empresas que foram fundadas após 1975 e que tenham mais de 2 milhões de doláres de valor de mercado
 
 const companies = [
-
-    { name:'Samsung',marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
-    
-    { name: 'Microsoft',marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975 },
-    
-    { name: 'Intel',marketValue: 117, CEO:'Brian Krzanich', foundedOn: 1968 },
-    
-    { name: 'Facebook',marketValue: 383, CEO:'Mark Zuckerberg', foundedOn: 2004 },
-    
-    { name: 'Spotify',marketValue: 30, CEO:'Daniel Ek', foundedOn: 2006 },
-    
+    { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975 },
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968 },
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004 },
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006 },
     { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976 }
-    
-    ];
+];
+
+// const filterCompanies = companies.filter((company) => {
+//     if(company.marketValue > 2 && company.foundedOn > 1975)
+//     return company
+// })
+
+const filterCompanies = companies.filter( company  => company.marketValue > 2 && company.foundedOn >= 1975 )
+
+
+
+console.log(filterCompanies)
