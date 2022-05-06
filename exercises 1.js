0a13247ec2319db97069e9ed030b26ae095dffb8
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2022-05-06 19:45:42 -0300

    first commit

---
+++ exercises 1.js
@@ -0,0 +1,7 @@
+// CRIAR UM NOVO ARRAY APENAS COM NÚMEROS PARES E DIVISÍVEIS POR 5 USANDO O FILTER
+
+const list = [20, 3, 234, 12, 17, 541, 6, 87, 275, 1000]
+
+const newList = list.filter((number) => {
+    
+})
---
+++ exercises 2.js
@@ -0,0 +1,17 @@
+// USAR O FILTER para identificar empresas que foram fundadas após 1975 e que tenham mais de 2 milhões de doláres de valor de mercado
+
+const companies = [
+
+    { name:'Samsung',marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
+    
+    { name: 'Microsoft',marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975 },
+    
+    { name: 'Intel',marketValue: 117, CEO:'Brian Krzanich', foundedOn: 1968 },
+    
+    { name: 'Facebook',marketValue: 383, CEO:'Mark Zuckerberg', foundedOn: 2004 },
+    
+    { name: 'Spotify',marketValue: 30, CEO:'Daniel Ek', foundedOn: 2006 },
+    
+    { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976 }
+    
+    ];
